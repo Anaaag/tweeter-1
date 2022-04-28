8bb463436d6e6fb6ba34a1af3199a9c1da2c81e8
Anaaag <agarc20@outlook.com>
2022-04-28 01:37:54 -0700

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#tweet-text").on('input', function () {
+    //  console.log($(this).val()); 
+    const maxChars = 140;
+    const length = $(this).val().length;
+    const $counter = $(this).siblings("div").children("output");
+    console.log("Counter", maxChars - length);
+    $counter.text(maxChars - length);
+    if (length > maxChars)  $counter.addClass("text-pink");
+    if (length <= maxChars) $counter.removeClass("text-pink")
+    
+
+    
+
+  });
+
+
+});
+
+
